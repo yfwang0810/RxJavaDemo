81bdf055880c1d7472726ad0e95374ec0f0b7fe7
王亚飞 <wangyafei@iscs.com.cn>
2017-04-09 23:09:56 +0800

    Rxjava Retrofit 学习使用

--- app/src/main/java/yfwang/rxjavademo/base/BaseApi.java
+++ app/src/main/java/yfwang/rxjavademo/base/BaseApi.java
@@ -1,15 +1,61 @@
 package yfwang.rxjavademo.base;
 
+import retrofit2.Retrofit;
+import rx.Observable;
+
 /**
  * Description: 请求数据统一封装
  * Copyright  : Copyright (c) 2016
  * Author     : yfwang
  * Date       : 2017/4/9 15:02
  */
-public class BaseApi {
+public abstract class BaseApi {
 
     /*超时时间-默认6秒*/
     private int connectionTime = 6;
+
+    /*请求的基础url*/
+    private String baseUrl = "www.baidu.com";
+
+    /* retry次数*/
+    private int retryCount = 1;
+    /*retry延迟*/
+    private long retryDelay = 100;
+    /*retry叠加延迟*/
+    private long retryIncreaseDelay = 100;
+
+    public int getRetryCount() {
+        return retryCount;
+    }
+
+    public void setRetryCount(int retryCount) {
+        this.retryCount = retryCount;
+    }
+
+    public long getRetryDelay() {
+        return retryDelay;
+    }
+
+    public void setRetryDelay(long retryDelay) {
+        this.retryDelay = retryDelay;
+    }
+
+    public long getRetryIncreaseDelay() {
+        return retryIncreaseDelay;
+    }
+
+    public void setRetryIncreaseDelay(long retryIncreaseDelay) {
+        this.retryIncreaseDelay = retryIncreaseDelay;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
 
     public int getConnectionTime() {
         return connectionTime;
@@ -19,5 +65,13 @@ public class BaseApi {
         this.connectionTime = connectionTime;
     }
 
+
+    /**
+     * 设置参数
+     *
+     * @param retrofit
+     * @return
+     */
+    public abstract Observable getObservable(Retrofit retrofit);
 
 }
--- app/src/main/java/yfwang/rxjavademo/manager/HttpManager.java
+++ app/src/main/java/yfwang/rxjavademo/manager/HttpManager.java
@@ -3,7 +3,12 @@ package yfwang.rxjavademo.manager;
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
 
 import java.lang.ref.SoftReference;
+import java.util.concurrent.TimeUnit;
 
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 import yfwang.rxjavademo.base.BaseApi;
 import yfwang.rxjavademo.listener.HttpOnNextListener;
 import yfwang.rxjavademo.listener.HttpOnNextSubListener;
@@ -34,7 +39,21 @@ public class HttpManager {
      * 处理http请求
      */
     public void doRequest(BaseApi api) {
+        //手动创建一个OkHttpClient并设置超时时间缓存等设置
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(api.getConnectionTime(), TimeUnit.SECONDS);
 
+         /*创建retrofit对象*/
+        Retrofit retrofit = new Retrofit.Builder().client(builder.build())
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .baseUrl(api.getBaseUrl()).build();
+
+//        Observable observable = api.getObservable(retrofit)
+//                //失败后retry配置
+//                .retryWhen(new RetryWhenNetworkException(api.getRetryCount(),
+//                        api.getRetryDelay(),api.getRetryIncreaseDelay()))
+//                .onErrorResumeNext();
 
 
     }
